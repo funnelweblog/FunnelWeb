04041a0948e49fa0c367d508ee903889df8434fd
mabsterama <none@none>
2010-07-22 21:05:17 +1000

    First try at committing. Early attempt at SyndicationFeed. Removed reference to "Paul Stovell" from master page. Some style changes just for me.

---
+++ src/FunnelWeb.Web/Application/ActionResults/FeedResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Text;
+
+namespace FunnelWeb.Web.Application.ActionResults
+{
+    public class FeedResult : ActionResult
+    {
+        public FeedResult(SyndicationFeedFormatter feed)
+        {
+            this._feed = feed;
+        }
+
+        public Encoding ContentEncoding { get; set; }
+        public string ContentType { get; set; }
+
+        private readonly SyndicationFeedFormatter _feed;
+        public SyndicationFeedFormatter Feed
+        {
+            get { return _feed; }
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var response = context.HttpContext.Response;
+            response.ContentType = !string.IsNullOrEmpty(ContentType) ? ContentType : "application/rss+xml";
+
+            if (ContentEncoding != null)
+            {
+                response.ContentEncoding = ContentEncoding;
+            }
+
+            if (_feed != null)
+            {
+                using (var xmlWriter = new XmlTextWriter(response.Output))
+                {
+                    xmlWriter.Formatting = Formatting.Indented;
+                    _feed.WriteTo(xmlWriter);
+                }
+            }
+        }
+    }
+
+}
