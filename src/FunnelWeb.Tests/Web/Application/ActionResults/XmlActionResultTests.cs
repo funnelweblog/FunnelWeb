a442f72c2db5934ff11f7b8ba6914e971e02bd01
slace <none@none>
2010-07-22 20:03:12 +1000

    Adding an XML action result and a unit test set for it Added a NUnit config

---
+++ src/FunnelWeb.Tests/Web/Application/ActionResults/XmlActionResultTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FunnelWeb.Web.Application.ActionResults;
+using System.Xml.Linq;
+
+namespace FunnelWeb.Tests.Web.Application.ActionResults
+{
+    [TestFixture] 
+    public class XmlActionResultTests
+    {
+        [Test]
+        public void XmlActionResultTests_Result_Content_Type_Is_Xml()
+        {
+            //Arrange
+            var actionResult = new XmlActionResult(new XDocument());
+
+            //Act
+
+            //Assert
+            Assert.AreEqual("text/xml", actionResult.ContentType);
+        }
+
+        [Test]
+        public void XmlActionResultTests_Cant_Pass_Null()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Throws(typeof(ArgumentNullException), () => new XmlActionResult(null));
+        }
+
+        [Test]
+        public void XmlActionResultTests_Default_Encoding_UTF8()
+        {
+            //Arrange
+            var actionResult = new XmlActionResult(new XDocument());
+            //Act
+
+            //Assert
+            Assert.AreEqual(System.Text.Encoding.UTF8, actionResult.Encoding);
+        }
+    }
+}
---
+++ src/FunnelWeb.Web/Application/ActionResults/XmlActionResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using System;
+
+namespace FunnelWeb.Web.Application.ActionResults
+{
+    public class XmlActionResult : ActionResult
+    {
+        public XDocument Xml { get; protected set; }
+        public string ContentType { get; set; }
+        public Encoding Encoding { get; set; }
+
+        public XmlActionResult(XDocument xml)
+        {
+            if (xml == null)
+                throw new ArgumentNullException("xml", "XML must be provided for this feed to use");
+            this.Xml = xml;
+            this.ContentType = "text/xml";
+            this.Encoding = Encoding.UTF8;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.ContentType = this.ContentType;
+            response.HeaderEncoding = this.Encoding;
+            XmlTextWriter writer = new XmlTextWriter(response.OutputStream, this.Encoding);
+            Xml.WriteTo(writer);
+            writer.Close();
+        }
+    }
+}
