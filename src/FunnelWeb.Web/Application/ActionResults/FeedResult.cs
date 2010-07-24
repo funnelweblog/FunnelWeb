340861a47871b5160018951c6268ec2f26651a51
slace <none@none>
2010-07-24 12:26:45 +1000

    Making images in feeds fully qualified

--- src/FunnelWeb.Web/Application/ActionResults/FeedResult.cs
+++ src/FunnelWeb.Web/Application/ActionResults/FeedResult.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using System.ServiceModel.Syndication;
-using System.Xml;
 using System.Text;
+using System.Web.Mvc;
+using System.Xml;
 
 namespace FunnelWeb.Web.Application.ActionResults
 {
@@ -28,17 +25,13 @@ namespace FunnelWeb.Web.Application.ActionResults
         public override void ExecuteResult(ControllerContext context)
         {
             if (context == null)
-            {
                 throw new ArgumentNullException("context");
-            }
 
             var response = context.HttpContext.Response;
             response.ContentType = !string.IsNullOrEmpty(ContentType) ? ContentType : "application/rss+xml";
 
             if (ContentEncoding != null)
-            {
                 response.ContentEncoding = ContentEncoding;
-            }
 
             if (_feed != null)
             {
