2a23ed6e3c55e1d5f251b23bf2243cc7c38cbb93
mabsterama <none@none>
2010-07-24 10:05:51 +1000

    Entry MetaKeywords are now surfaced as feed categories

--- src/FunnelWeb.Web/Controllers/FeedController.cs
+++ src/FunnelWeb.Web/Controllers/FeedController.cs
@@ -52,26 +52,38 @@ namespace FunnelWeb.Web.Controllers
         {
             var entries = _feedRepository.GetFeed(feedName, 0, 20);
 
-            var items = from e in entries
-                        let itemUri = new Uri(Request.Url, Url.Action("Page", "Wiki", new { page = e.Name }))
-                        select new SyndicationItem
-                        {
-                            Id = itemUri.ToString(),
-                            Title = TextSyndicationContent.CreatePlaintextContent(e.Title),
-                            Summary = TextSyndicationContent.CreateHtmlContent(
-                                _markdown.Render(e.LatestRevision.Body) + String.Format("<img src=\"{0}\" />", itemUri + "/via-feed")),
-                            LastUpdatedTime = e.LatestRevision.Revised,
-                            Links = 
-                            {
-                                new SyndicationLink(itemUri) 
-                            },
-                            Authors = 
-                            {
-                                new SyndicationPerson { Name = _settings.Author } 
-                            },
-                        };
+            var items = (from e in entries
+                         let itemUri = new Uri(Request.Url, Url.Action("Page", "Wiki", new { page = e.Name }))
+                         select new
+                         {
+                             Item = new SyndicationItem
+                             {
+                                 Id = itemUri.ToString(),
+                                 Title = TextSyndicationContent.CreatePlaintextContent(e.Title),
+                                 Summary = TextSyndicationContent.CreateHtmlContent(
+                                     _markdown.Render(e.LatestRevision.Body) + String.Format("<img src=\"{0}\" />", itemUri + "/via-feed")),
+                                 LastUpdatedTime = e.LatestRevision.Revised,
+                                 Links = 
+                                {
+                                    new SyndicationLink(itemUri) 
+                                },
+                                 Authors = 
+                                {
+                                    new SyndicationPerson { Name = _settings.Author } 
+                                },
+                             },
+                             Keywords = e.MetaKeywords.Split(',')
+                         }).ToList();
 
-            return FeedResult(items);
+            foreach (var item in items)
+            {
+                foreach (var keyword in item.Keywords)
+                {
+                    item.Item.Categories.Add(new SyndicationCategory(keyword));
+                }
+            }
+
+            return FeedResult(items.Select(i => i.Item));
         }
 
         public ActionResult CommentFeed()
