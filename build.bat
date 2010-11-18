aef32791a1881fea5731007b9f6cce41329a14f3
paulstovell <none@none>
2010-11-18 22:58:39 +0800

    Created a build.bat that produces the ZIP files;

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set appversion=1.0.0.0
+set framework=v4.0.30319
+
+"%SystemDrive%\Windows\Microsoft.NET\Framework\%framework%\MSBuild.exe" build\Build.proj /t:Build /p:build_number=%appversion% /v:m
+echo Done
+pause
---
+++ src/Information/SolutionInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xstrata")]
+[assembly: AssemblyProduct("Xstrata Training Kiosk")]
+[assembly: AssemblyCopyright("Copyright © Xstrata 2010")]
+[assembly: ComVisible(false)]
