1156f0163f353bb09ec090990e8b421731cda09e
paulstovell <none@none>
2011-01-17 18:49:53 +1000

    Big changes, see: http://groups.google.com/group/funnelweblog/browse_thread/thread/90c7195b81dad17

---
+++ src/FunnelWeb/Model/Repositories/IFeedRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FunnelWeb.Model.Repositories
+{
+    public interface IFeedRepository
+    {
+        int GetEntryCount();
+        IEnumerable<Entry> GetRecentEntries(int skip, int take);
+        
+        IEnumerable<Comment> GetRecentComments(int skip, int take);
+    }
+}
