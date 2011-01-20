274453ed9fe01d9fd8fe8911eb2d42d8dae57248
slace <none@none>
2011-01-20 20:35:18 +1100

    pulling data from server now works

--- src/FunnelWeb.Web/Content/Scripts/Site/taggy.js
+++ src/FunnelWeb.Web/Content/Scripts/Site/taggy.js
@@ -1,16 +1,24 @@
-﻿$(function () {
+﻿/// <reference path="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4-vsdoc.js" />
+$(function () {
 
-    var skillsDb = [
-            { value: "Push", name: "Push" },
-            { value: "Hit", name: "Hit" },
-            { value: "Trap", name: "Trap" },
-            { value: "Flick", name: "Flick" }
-        ];
+    var tagCache = {};
 
     $("#tagInput").autocomplete({
 
         minLength: 0,
-        source: skillsDb,
+        source: function (request, response) {
+            if (tagCache.hasOwnProperty(request.term)) {
+                response(tagCache[request.term]);
+            } else {
+                $.getJSON('/tag/' + request.term, function (data) {
+                    tagCache[request.term] = $.map(data, function (item) {
+                        return { label: item.Name, value: item.Id, id: item.Id };
+                    });
+                    console.log(response);
+                    response(tagCache[request.term]);
+                });
+            }
+        },
 
         focus: function (event, ui) {
             $("#tagInput").val(ui.item.name);
@@ -19,7 +27,7 @@
 
         select: function (event, ui) {
             $('.newTagInputWrapper').before(
-                    '<li class="newTagItem" id="container_' + ui.item.value + '"><a><em class="tagName">' + ui.item.name + '</em><span class="rm" id="' + ui.item.value + '"></span></a></li>'
+                    '<li class="newTagItem" id="container_' + ui.item.value + '"><a><em class="tagName">' + ui.item.label + '</em><span class="rm" id="' + ui.item.value + '"></span></a></li>'
                 );
         },
 
@@ -35,8 +43,6 @@
         if ($(event.target).is('span')) {
             $('#container_' + event.target.id).remove();
         }
-
-
     });
 
     $('#tags').click(function (event) {
