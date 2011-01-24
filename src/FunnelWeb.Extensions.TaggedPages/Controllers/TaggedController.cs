baa7b40ef441482b08bd84f8e31c663a1bc238aa
slace <none@none>
2011-01-24 13:51:09 +1100

    starting work on an example controller plugin for displaying posts by tags

---
+++ src/FunnelWeb.Extensions.TaggedPages/Controllers/TaggedController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Autofac;
+
+namespace FunnelWeb.Extensions.TaggedPages.Controllers
+{
+    [FunnelWebExtension(FullName = "Display posts by tags", Publisher = "FunnelWeb", SupportLink = "http://www.funnelweblog.com")]
+    public class TaggedController : Controller, IRoutableFunnelWebExtension
+    {
+        void IFunnelWebExtension.Initialize(ContainerBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        RouteCollection IRoutableFunnelWebExtension.Routes { get; set; }
+    }
+}
