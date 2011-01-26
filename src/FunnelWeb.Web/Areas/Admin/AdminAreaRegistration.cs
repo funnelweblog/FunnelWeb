74e2fa9ce6733d1f603f06cbbbfb94ea8ef95002
slace <none@none>
2011-01-26 21:14:18 +1100

    refactoring the routes
    --HG--
    branch : admin-area-refactor

--- src/FunnelWeb.Web/Areas/Admin/AdminAreaRegistration.cs
+++ src/FunnelWeb.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -2,23 +2,53 @@
 
 namespace FunnelWeb.Web.Areas.Admin
 {
-	public class AdminAreaRegistration : AreaRegistration
-	{
-		public override string AreaName
-		{
-			get
-			{
-				return "Admin";
-			}
-		}
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
 
-		public override void RegisterArea(AreaRegistrationContext context)
-		{
-			context.MapRoute(
-				"Admin_default",
-				"admin/{controller}/{action}/{id}",
-				new {  controller = "admin", action = "Index", id = UrlParameter.Optional }
-			);
-		}
-	}
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_Login",
+                "admin/login/{action}",
+                new { controller = "login", action = "login" }
+                );
+
+            context.MapRoute(
+                "Admin_Settings",
+                "admin/settings/{action}",
+                new { controller = "Settings", action = "Index" }
+                );
+
+            context.MapRoute(
+                "Admin_comments",
+                "admin/comments/{action}",
+                new { controller = "Comments", action = "Index" }
+                );
+
+            context.MapRoute(
+                "Admin_Install",
+                "admin/install/{action}",
+                new {controller = "Install", action = "Index"}
+                );
+
+            context.MapRoute(
+                "Admin_Wiki",
+                "admin/{controller}/{action}/{*page}",
+                new { controller = "WikiAdmin", action = "Edit", page = UrlParameter.Optional }
+                );
+
+            context.MapRoute(
+                "Admin_default",
+                "admin/{controller}/{action}/{id}",
+                new { controller = "admin", action = "Index", id = UrlParameter.Optional }
+                );
+        }
+    }
 }
