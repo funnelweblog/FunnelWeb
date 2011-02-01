6444758ea3e4e30364f0a6bd2c078eaa1a754242
JakeGinnivan <none@none>
2011-02-01 20:34:46 +0800

    Json working

---
+++ src/FunnelWeb.Extensions.WcfDemo/Services/JsonService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace FunnelWeb.Extensions.WcfDemo.Services
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class JsonService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "getdata", ResponseFormat = WebMessageFormat.Json)]
+        public List<string> GetDemoData()
+        {
+            return new List<string>
+                       {
+                           "Result1",
+                           "Result2"
+                       };
+        }
+    }
+}
--- src/FunnelWeb.Extensions.WcfDemo/WcfDemoBootstrapper.cs
+++ src/FunnelWeb.Extensions.WcfDemo/WcfDemoBootstrapper.cs
@@ -1,5 +1,10 @@
-﻿using Autofac;
+﻿using System.Collections.Generic;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using Autofac;
 using FunnelWeb.Extensions.WcfDemo.Services;
+using Microsoft.ServiceModel.Description;
+using Microsoft.ServiceModel.Dispatcher;
 using Microsoft.ServiceModel.Http;
 
 namespace FunnelWeb.Extensions.WcfDemo
@@ -10,6 +15,24 @@ namespace FunnelWeb.Extensions.WcfDemo
         public override void Initialize(ContainerBuilder builder)
         {
             Routes.AddServiceRoute<DemoService>("services/demoservice", new HttpHostConfiguration());
+            Routes.AddServiceRoute<JsonService>("services/jsonservice", new JsonValueSampleConfiguration());
         }
     }
+
+     public class JsonValueSampleConfiguration : HttpHostConfiguration, IProcessorProvider
+    {
+        public void RegisterRequestProcessorsForOperation(HttpOperationDescription operation, IList<Processor> processors, MediaTypeProcessorMode mode)
+        {
+            processors.Add(new FormUrlEncodedProcessor(operation, mode));
+            processors.Add(new JsonProcessor(operation, mode));
+
+        }
+        public void RegisterResponseProcessorsForOperation(HttpOperationDescription operation, IList<Processor> processors, MediaTypeProcessorMode mode)
+        {
+            processors.ClearMediaTypeProcessors();
+            processors.Add(new JsonProcessor(operation, mode));
+        }
+    }
+
+
 }
