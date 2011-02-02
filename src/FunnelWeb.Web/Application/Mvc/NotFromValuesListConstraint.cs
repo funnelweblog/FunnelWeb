998401dc4e3822bee2071024d1ed30b5f2391ff8
paulstovell <none@none>
2011-02-02 22:18:12 +1000

    Moved this class into its own file

---
+++ src/FunnelWeb.Web/Application/Mvc/NotFromValuesListConstraint.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace FunnelWeb.Web.Application.Mvc
+{
+    public class NotFromValuesListConstraint : IRouteConstraint
+    {
+        private readonly string[] _values;
+
+        public NotFromValuesListConstraint(params string[] values)
+        {
+            _values = values;
+        }
+
+        public bool Match(HttpContextBase httpContext,
+                          Route route,
+                          string parameterName,
+                          RouteValueDictionary values,
+                          RouteDirection routeDirection)
+        {
+            if (!values.ContainsKey(parameterName))
+                return true;
+
+            // Get the value called "parameterName" from the 
+            // RouteValueDictionary called "value"
+            string value = values[parameterName].ToString();
+
+            // Return true is the list of allowed values contains 
+            // this value.
+            var match = !_values.Any(value.Contains);
+            return match;
+        }
+    }
+}
--- src/FunnelWeb.Web/RoutesModule.cs
+++ src/FunnelWeb.Web/RoutesModule.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.ServiceModel.Activation;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Autofac;
@@ -86,35 +85,6 @@ namespace FunnelWeb.Web
             {
                 routes.Add(serviceRoute);
             }
-        }
-    }
-
-    public class NotFromValuesListConstraint : IRouteConstraint
-    {
-        private readonly string[] _values;
-
-        public NotFromValuesListConstraint(params string[] values)
-        {
-            _values = values;
-        }
-
-        public bool Match(HttpContextBase httpContext,
-        Route route,
-        string parameterName,
-        RouteValueDictionary values,
-        RouteDirection routeDirection)
-        {
-            if (!values.ContainsKey(parameterName))
-                return true;
-
-            // Get the value called "parameterName" from the 
-            // RouteValueDictionary called "value"
-            string value = values[parameterName].ToString();
-
-            // Return true is the list of allowed values contains 
-            // this value.
-            var match = !_values.Any(value.Contains);
-            return match;
         }
     }
 }
