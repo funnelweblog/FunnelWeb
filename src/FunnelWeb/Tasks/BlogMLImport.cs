281acc42042104a59d73c06ff7f8ccf69bf01390
paulstovell <none@none>
2011-04-07 23:34:52 +1000

    Removed duplicated classes.

--- src/FunnelWeb/Tasks/BlogMLImport.cs
+++ src/FunnelWeb/Tasks/BlogMLImport.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
 using BlogML;
 using BlogML.Xml;
 using FunnelWeb.Authentication;
