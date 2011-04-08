04576140526eaea1326534f95b85ab42dfada5a0
paulstovell <none@none>
2011-04-08 23:14:54 +1000

    Tests for how we manage transactions

--- src/FunnelWeb/Filters/FunnelWebRequestAttribute.cs
+++ src/FunnelWeb/Filters/FunnelWebRequestAttribute.cs
@@ -8,9 +8,21 @@ namespace FunnelWeb.Filters
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class FunnelWebRequestAttribute : ActionFilterAttribute
     {
+        private readonly Func<ISession> sessionFinder;
+
+        public FunnelWebRequestAttribute()
+        {
+            sessionFinder = () => DependencyResolver.Current.GetService<ISession>();
+        }
+
+        public FunnelWebRequestAttribute(Func<ISession> sessionFinder)
+        {
+            this.sessionFinder = sessionFinder;
+        }
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var session = DependencyResolver.Current.GetService<ISession>();
+            var session = sessionFinder();
             session.BeginTransaction(IsolationLevel.Serializable);
         }
 
@@ -18,13 +30,13 @@ namespace FunnelWeb.Filters
         {
             if (filterContext.Exception == null)
             {
-                var session = DependencyResolver.Current.GetService<ISession>();
+                var session = sessionFinder();
                 session.Flush();
                 session.Transaction.Commit();
             }
             else
             {
-                var session = DependencyResolver.Current.GetService<ISession>();
+                var session = sessionFinder();
                 session.Clear();
                 session.Transaction.Rollback();
             }
