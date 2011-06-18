51cb725243ef7047e7e50e80f3efe18b76be5bbe
Jake@spaceship <Jake@spaceship>
2011-06-18 10:27:51 +0800

    Got rid of some old projects which are not used

--- src/FunnelWeb.DatabaseDeployer/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("FunnelWeb.DatabaseDeployer")]
-[assembly: AssemblyDescription("Contains database deployment scripts for the FunnelWeb blog engine")]
