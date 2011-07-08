adde9b98824ad4814d642b9edad1ef76c15f0daa
Jake Ginnivan <jake@ginnivan.net>
2011-07-08 15:19:59 +0800

    Updated nHibernate to latest version

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.2.571")]
-[assembly: AssemblyFileVersion("2.0.2.571")]
+[assembly: AssemblyVersion("2.0.2.572")]
+[assembly: AssemblyFileVersion("2.0.2.572")]
 
 
