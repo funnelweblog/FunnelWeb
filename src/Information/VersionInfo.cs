2ed1e1b37d57c92466fb0f3d2d7d0345b4de50d4
Jake Ginnivan <jake@ginnivan.net>
2011-07-13 20:20:16 +0800

    Fixed build, now properly builds sql ce
    --HG--
    branch : SqlCe

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.2.581")]
-[assembly: AssemblyFileVersion("2.0.2.581")]
+[assembly: AssemblyVersion("2.0.2.582")]
+[assembly: AssemblyFileVersion("2.0.2.582")]
 
 
