c00a329be6aa12dbd0cd2720440159325c422de9
Jake Ginnivan <jake@ginnivan.net>
2011-07-14 12:45:07 +0800

    Updated Change Log with features added so far for the v2.1 release

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.2.582")]
-[assembly: AssemblyFileVersion("2.0.2.582")]
+[assembly: AssemblyVersion("2.1.0.585")]
+[assembly: AssemblyFileVersion("2.1.0.585")]
 
 
