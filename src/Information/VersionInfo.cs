2a96897829866ba95a8ddf0e2b4a66e0a1e27922
Jake Ginnivan <jake@ginnivan.net>
2011-07-21 10:34:58 +0800

    Updated change log again

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.1.0.585")]
-[assembly: AssemblyFileVersion("2.1.0.585")]
+[assembly: AssemblyVersion("2.1.0.591")]
+[assembly: AssemblyFileVersion("2.1.0.591")]
 
 
