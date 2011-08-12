62962a14faf1b6aeb6c88f93e4f8020d5086b02c
TheBlueSky <none@none>
2011-08-12 15:42:25 +0400

    Fixing few broken links (specially if FunnelWeb was not installed on the web site root; e.g. http://somedomain/funnelweb)

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.1.0.595")]
-[assembly: AssemblyFileVersion("2.1.0.595")]
+[assembly: AssemblyVersion("2.1.0.596")]
+[assembly: AssemblyFileVersion("2.1.0.596")]
 
 
