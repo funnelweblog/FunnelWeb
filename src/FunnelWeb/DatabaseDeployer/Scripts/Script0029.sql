a08ff2e7b221a884b910ee99854f92b88dd5c0fd
Jake Ginnivan <jake@ginnivan.net>
2011-09-07 10:56:20 +0800

    Updated Wallace's upgrade script to support schemas

--- src/FunnelWeb/DatabaseDeployer/Scripts/Script0029.sql
+++ src/FunnelWeb/DatabaseDeployer/Scripts/Script0029.sql
@@ -1,6 +1,4 @@
-﻿--Script 24 changes type to varchar(100), it should be nvarchar(100). 
---SqlCe version adds column so these alter columns don't do anything
-alter table $schema$.[Entry] alter column [Author] nvarchar(100) not null
+﻿alter table $schema$.[Entry] alter column [Author] nvarchar(100) not null
 go
 
 alter table $schema$.[Entry] alter column [RevisionAuthor] nvarchar(100) not null
--- src/FunnelWeb/DatabaseDeployer/Scripts/Script0030.sql
+++ src/FunnelWeb/DatabaseDeployer/Scripts/Script0030.sql
@@ -1,2 +1,3 @@
-﻿UPDATE [Tag] SET Name = REPLACE(Name, ' ', '')
+﻿UPDATE $schema$.[Tag] SET Name = REPLACE(Name, ' ', '')
+UPDATE $schema$.[Entry] SET [TagsCommaSeparated] = REPLACE([TagsCommaSeparated], ' ', '')
 go
