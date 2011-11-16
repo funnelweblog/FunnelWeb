eec7a6aea8051e8fc27561ad4bcd256bce3c1585
slace <me@aaron-powell.com>
2011-11-16 14:27:24 +1100

    new build

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.1.0.641")]
-[assembly: AssemblyFileVersion("2.1.0.641")]
+[assembly: AssemblyVersion("2.1.0.644")]
+[assembly: AssemblyFileVersion("2.1.0.644")]
 
 
