fddd165316d77cfbda5cd2db612f63e17e91cb2f
slace <me@aaron-powell.com>
2011-11-19 09:25:16 +1100

    Refactoring paginator from being in App_Code to a proper helper

---
+++ src/FunnelWeb.Web/Models/Paginator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FunnelWeb.Web.Models
+{
+    public class Paginator
+    {
+        public string ActionName { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
