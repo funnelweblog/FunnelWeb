40eb391f3c28578821936835037b37e5c5a18830
slace <me@aaron-powell.com>
2012-02-29 09:30:35 -0800

    fixing disqus comments on tagged listing page

---
+++ src/FunnelWeb.Web/Scripts/disqus-count.js
@@ -0,0 +1,6 @@
+﻿(function (window, document) {
+    var s = document.createElement('script'); s.async = true;
+    s.type = 'text/javascript';
+    s.src = 'http://' + window.disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+})(window, document);
