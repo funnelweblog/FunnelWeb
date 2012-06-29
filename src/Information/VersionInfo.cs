fbcd9bea882009666736f42a040bfc9d450c4829
JakeGinnivan <jake@ginnivan.net>
2012-06-29 14:15:24 +0800

    Made the web deploy zip path a bit nicer

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.2.0.753")]
-[assembly: AssemblyFileVersion("2.2.0.753")]
+[assembly: AssemblyVersion("2.2.0.754")]
+[assembly: AssemblyFileVersion("2.2.0.754")]
 
 
