5b1be31831fbd8c367d5d4a90ce221c0e29716b7
Andras Fuchs <andras.fuchs@gmail.com>
2015-03-30 10:08:44 +0200

    Updated version info to 2.5
    Updated version info to 2.5

--- src/Information/VersionInfo.cs
+++ src/Information/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.4.*")]
-[assembly: AssemblyFileVersion("2.4.*")]
+[assembly: AssemblyVersion("2.5.*")]
+[assembly: AssemblyFileVersion("2.5.*")]
 
 
